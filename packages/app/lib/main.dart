9859e9b3e7adb166fdad1efb8140592761f02d8f
salan70 <tetsuo21ad@gmail.com>
2024-06-16 16:02:32 +0900

    feat: :hammer: common パッケージをほかプロジェクトから流用

--- packages/app/lib/main.dart
+++ packages/app/lib/main.dart
@@ -1,7 +1,25 @@
+import 'package:common/common.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 画面の向きを縦で固定する。
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(
+      ProviderScope(
+        overrides: [
+          flavorProvider.overrideWithValue(Flavor.fromEnvironment),
+        ],
+        child: const MainApp(),
+      ),
+    );
+  });
 }
 
 class MainApp extends StatelessWidget {
